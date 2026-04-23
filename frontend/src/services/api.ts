2ef455006878830d1e179a76c12909770e25f7f1
Silki Rai <silkyrai18@gmail.com>
2026-04-23 21:09:43 +0530

    some change

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api",
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export const setAuthToken = (token: string) => {
